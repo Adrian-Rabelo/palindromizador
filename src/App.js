0a173b04e7a3e5b5a64d66ae58100002d3aed734
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-07-09 15:22:12 -0400

    expressoes em jsx, inserção de imagens no App, constantes e variáveis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import Pinto from "./components/img/pinto-transp.png";
 export default function App() {
+    const prog = () => {
+        return "Rabelinho Dev";
+    };
+    function curso() {
+        return "Curso de React";
+    }
     return (
-        <>
-            <p>Rabelinho Dev</p>
-            <br />
-            <p>Curso de React</p>
-        </>
+        <section>
+            <header>
+                <p>{"Programador: " + prog()}</p>
+                <br />
+                <p>{"Curso: " + curso()}</p>
+            </header>
+            <section>
+                <img src={Pinto} />
+                <img src="/img/euzim-transp.png" />
+            </section>
+        </section>
     );
 }
