48ad2fe282a5b76c697a9c1cd85f6498a29ae808
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-07-09 17:59:03 -0400

    aplicando estilos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Header from "./components/Header.js";
-import Corpo from "./components/Corpo.js";
-import Dados from "./components/Dados.js";
-
+import "./App.css";
 export default function App() {
+    const textoDestaque = {
+        color: "#00f",
+        fontSize: "3em",
+    };
     return (
         <>
-            <Header />
-            <Corpo />
+            <section className="caixa">
+                <h1 style={{ color: "#f00", fontSize: "5em" }}>
+                    Rabelinho Dev
+                </h1>
+                <h2 style={textoDestaque}>Curso de React</h2>
+                <p className="texto">GitHub: github.com/adrian-rabelo</p>
+                <a
+                    href="https://www.example.com"
+                    rel="noreferrer"
+                    target="_blank">
+                    Dev Rabelin
+                </a>
+            </section>
         </>
     );
 }
