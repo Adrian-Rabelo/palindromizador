f43db24bd2c775b75ad340e22b0273b6e3263673
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-07-09 22:13:48 -0400

    praticando

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
 import "./App.css";
 import Numero from "./components/Numero.js";
+import PalavraRevertida from "./components/PalavraRevertida";
 
 export default function App() {
     let [color, setColor] = useState("");
-    let [text, setText] = useState("");
-    let texto = document.getElementById("texto");
+    let [input, setInput] = useState("");
+    let [pReverso, setPReverso] = useState("");
+    function reverterPalavra(palavra) {
+        return palavra.split("").reverse().join("");
+    }
+    function teste() {
+        return <PalavraRevertida palavraRevertida={reverterPalavra(input)} />;
+    }
+
     return (
         <>
-            <section>
-                <h1>Texto atualizador</h1>
-                <input
-                    type="text"
-                    id="texto"
-                    onChange={() => {
-                        setText(texto.value);
-                    }}
-                />
-            </section>
-            <section>
-                <div>
-                    <h1>
-                        <p>Seu texto: {text}</p>
-                    </h1>
-                </div>
+            <section className="geral">
+                <section>
+                    <div className="textoEntrada">
+                        <h1>Texto atualizador</h1>
+                        <input
+                            type="text"
+                            id="texto"
+                            onChange={(event) => setInput(event.target.value)}
+                        />
+                        <button className="reverter">Reverter !</button>
+                    </div>
+                </section>
+                <section>
+                    <div className="textoEscrito">
+                        <h1>
+                            <p>Seu texto</p>|<br />v<p>{input}</p>
+                            <PalavraRevertida
+                                palavraRevertida={reverterPalavra(input)}
+                            />
+                        </h1>
+                    </div>
+                </section>
             </section>
         </>
     );
---
+++ src/components/PalavraRevertida.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function PalavraRevertida(props) {
+    return (
+        <section>
+            <p>{props.palavraRevertida}</p>
+        </section>
+    );
+}
